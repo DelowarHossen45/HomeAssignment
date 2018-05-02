51bc6ddc30473ebdb5439d987c2f45ec09eedd0c
Ahad <ahadsheikh1999@gmail.com>
2018-05-02 19:44:11 +0600

    my first commit #ahad

--- assignmen.cpp
+++ assignmen.cpp
@@ -1,17 +1,19 @@
 #include<bits/stdc++.h>
 using namespace std;
-<<<<<<< HEAD
+
+void sub(int x,int y){
+	return x-y;
+}
+
 void sum(int a ,int b){
 	cout<<"Enter Two Intiger :";
 	cin>>a>>b;
 	cout<<"Sum = "<<a+b<endl;
 }
 
-inr main()
-=======
 int main()
->>>>>>> master
 {
     sum();
 	
 }
+
