88bb876ae385a14c550ed0a1002b061ac0009d6a
Apple Mahmud <applecse25@gmail.com>
2018-05-02 19:40:12 +0600

    secound1

--- assignmen.cpp
+++ assignmen.cpp
@@ -1,16 +1,12 @@
 #include<bits/stdc++.h>
-using namespace std;
-<<<<<<< HEAD
 void sum(int a ,int b){
+	
 	cout<<"Enter Two Intiger :";
 	cin>>a>>b;
 	cout<<"Sum = "<<a+b<endl;
 }
 
-inr main()
-=======
 int main()
->>>>>>> master
 {
     sum();
 	
